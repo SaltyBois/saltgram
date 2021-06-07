f89652b6c0673f288e58bf158ef6879778b2bf10
Kris98Wolf <kristian98farkas@gmail.com>
2021-06-07 17:10:08 +0200

    :zap: Started content microservice

---
+++ src/content/main.go
@@ -0,0 +1,24 @@
+package content
+
+import (
+	"log"
+	"os"
+
+	"saltgram/internal"
+)
+
+func main() {
+	l := log.New(os.Stdout, "saltgram-contents", log.LstdFlags)
+	l.Printf("Starting Content microservice on port: %s\n", os.Getenv("SALT_CONTENT_PORT"))
+	s := internal.NewService(l)
+
+	db := data.DBConn{}
+	db.ConnectToDb()
+	db.MigradeData()
+	err := s.TLS.Init("../../certs/localhost.crt", "../../certs/localhost.key", "../../certs/RootCA.pem")
+	if err != nil {
+		l.Fatalf("[ERROR] configuring TLS: %v\n", err)
+	}
+
+
+}
---
+++ src/protos/content/prcontent/content.pb.go
@@ -0,0 +1,15 @@
+package prcontent
+
+import (
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	reflect "reflect"
+	sync "sync"
+)
+
+const (
+	// Verify that this generated code is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(20 - protoimpl.MinVersion)
+	// Verify that runtime/protoimpl is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
+)
