e3c8ffa71bec6403ede1cc1f4730f7d6feb536e5
L4v <ivosevic.jovan@uns.ac.rs>
2021-06-20 13:56:39 +0200

    :bug:Updated decrypt.sh

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir $HOME/secrets
+
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
 --output $HOME/secrets/saltgram-c0751de619fa.json secrets/saltgram-c0751de619fa.json.gpg
 
